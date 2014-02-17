b17e18786b22c99e56c8ffdc33bdd4c7ed0b08b5
Rosie Hoyem <rosiehoyem@gmail.com>
2014-02-17 00:28:24 -0600

    updated round show page

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,6 @@ PhotoContest::Application.routes.draw do
     end
   end
 
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'contests#index'
 
   # Example of regular route:
